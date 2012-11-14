5202a5b6be28c06471401c92458edc93cfab85be
Erin Rasmussen <rasmusse@ubuntu.(none)>
2012-11-14 08:44:50 -0600

    create.C compiles now

--- part5/create.C
+++ part5/create.C
@@ -15,7 +15,33 @@ const Status RelCatalog::createRel(const string & relation,
   if (relation.length() >= sizeof rd.relName)
     return NAMETOOLONG;
 
-
+  
+  // make sure that a relation with the same name doesn't already exist
+  if ((status = this->getInfo(relation, rd)) == OK) return RELEXISTS;
+  
+  // fill in the RelDesc and add to relCat. Similar to dbcreate.C
+  strcpy(rd.relName, relation.c_str());
+  rd.attrCnt = attrCnt;
+  if ((status = relCat->addInfo(rd)) != OK) return status;
+  
+  // fill the AttrDesc with attributes like in dbcreate.C
+  for (int i = 0; i < attrCnt; i++) {
+        strcpy(ad.attrName, attrList[i].attrName);
+        strcpy(ad.relName, attrList[i].relName);
+        ad.attrLen = attrList[i].attrLen;
+        ad.attrType = attrList[i].attrType;
+		if (i == 0) {
+           ad.attrOffset = i;
+		} else {
+			ad.attrOffset += sizeof attrList[i-1];
+		}
+        if ((status = attrCat->addInfo(ad)) != OK) break;
+    }
+  
+    // create a HeapFile instance to hold tuples of the relation
+    status = createHeapFile(relation);
+    return status;
+    
 
 
 
