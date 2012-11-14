1caf0b10ab3888995d8e74b225c45e2dc5c0b3e4
Erin Rasmussen <rasmusse@ubuntu.(none)>
2012-11-14 09:16:37 -0600

    destroy.C compiles and should be working

--- part5/destroy.C
+++ part5/destroy.C
@@ -14,14 +14,41 @@
 const Status RelCatalog::destroyRel(const string & relation)
 {
   Status status;
-
+  RelDesc rd;
+  AttrDesc* ad;
+  
   if (relation.empty() || 
       relation == string(RELCATNAME) || 
       relation == string(ATTRCATNAME))
     return BADCATPARM;
-
-
-
+	
+  // Get meta data of the relation
+  if ((status = relCat->getInfo(relation, rd)) != OK) return status;
+  
+  // Get all attributes of the relation
+  if ((status = attrCat->getRelInfo(relation, rd.attrCnt, ad)) != OK){
+	  delete[] ad;
+	  return status;
+  }
+  
+  // Delete all attributes of the relation
+  for (int i = 0; i < rd.attrCnt; i++) {
+        status = attrCat->removeInfo(relation, ad[i].attrName);
+        if (status != OK) {
+            delete[] ad;
+            return status;
+        }
+  }
+  
+  // getRelInfo doesn't deallocate this
+  delete[] ad;
+  
+  // remove the relation
+  if ((status = relCat->removeInfo(relation)) != OK) return status;
+  
+  // destroy the HeapFile
+  status = destroyHeapFile(relation);
+  return status;
 
 
 }
@@ -40,13 +67,45 @@ const Status RelCatalog::destroyRel(const string & relation)
 const Status AttrCatalog::dropRelation(const string & relation)
 {
   Status status;
-  AttrDesc *attrs;
+  AttrDesc *ad;
+  RelDesc rd;
   int attrCnt, i;
 
   if (relation.empty()) return BADCATPARM;
-
-
-
+ 
+ // Get meta data of the relation
+  if ((status = relCat->getInfo(relation, rd)) != OK) return status;
+  
+  // Get all attributes of the relation
+  if ((status = attrCat->getRelInfo(relation, rd.attrCnt, ad)) != OK){
+	  delete[] ad;
+	  return status;
+  }
+  
+  // Delete all attributes of the relation
+  for (int i = 0; i < rd.attrCnt; i++) {
+        status = attrCat->removeInfo(relation, ad[i].attrName);
+        if (status != OK) {
+            delete[] ad;
+            return status;
+        }
+  }
+  
+  // Update the meta data in relcat for this relation
+  
+  // getRelInfo doesn't deallocate this
+  delete[] ad;
+  
+  // Remove the record from the relCat table
+  if ((status = relCat->removeInfo(relation)) != OK) return status;
+				
+  // Set the attribute count to 0 because we are deleting all attributes
+  // from that relation
+  rd.attrCnt = 0;
+				
+  // Add back to the table -- by deleting and then adding we
+  // have updated the record
+  if ((status = relCat->addInfo(rd)) != OK) return status;
 
 
 }
