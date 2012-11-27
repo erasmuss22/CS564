c09097823fa11e6bb64842bea131bc1b6e31d181
Erin Rasmussen <rasmusse@ubuntu.(none)>
2012-11-26 22:32:45 -0600

    delete might be done?

--- part6/delete.C
+++ part6/delete.C
@@ -17,7 +17,67 @@ const Status QU_Delete(const string & relation,
 		       const char *attrValue)
 {
 // part 6
-return OK;
+	Status status;
+	RID rid;
+	const char *filter;
+	AttrDesc ad;
+	
+	HeapFileScan* hfs = new HeapFileScan(result, status);
+    if(status != OK) {
+		delete hfs;
+		return status;
+	}
+	
+	if (attrValue != NULL){
+		
+		// Get and store the AttrDesc for the relName and attrName of the attr
+		if((status = attrCat->getInfo(relation, attrName, ad)) != OK) { 
+			return status;
+		}
+		
+		// get the attribute type and store it as a filter
+		switch(ad.attrType){
+			case FLOAT:
+			
+			    float tempf = atof(attrValue);
+			    filter = (char *)&tempf;
+			    break;
+				
+		    case INTEGER:
+			
+			    int tempi = atoi(attrValue);
+			    filter = (char *)&tempi;
+			    break;
+				
+		    default:
+			    filter = attrValue;
+			    break;
+	    }
+		
+	}
+	
+
+	if ((status = hfs->startScan(ad.attrOffset, ad.attrLen, type, filter, op)) != OK){
+		delete hfs;
+		return status;
+	}
+	
+	while((status = hfs.scanNext(rid)) == OK){
+		
+		if((status = hfs.deleteRecord()) != OK) {
+			delete hfs;
+			return status;
+		}
+		
+		
+	}
+
+	if (status != FILEEOF) { 
+		delete hfs;
+		return status;
+	}
+	delete hfs;
+	return OK;
 
 
 
